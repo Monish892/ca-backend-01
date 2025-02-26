116a0a3b6262ee594f765bd4eb8fa84d65261845
Monish892 <jayachitraramesh08@gmail.com>
2025-02-26 09:26:39 +0530

    files added

---
+++ server.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const port=3000;
+const app=express()
+
+app.use(express.json())
+
+
+app.listen(port,()=>{
+
+    console.log(`server is running on the port ${port}`);
+    
+})
+
+
+
+const users={
+    john:{name:'john doe',age:30,email:'john@gmail.com'},
+    jane:{name:'jane smith',age:25,email:'jane@gmail.com'},
+    peter:{name:'peter jones',age:35,email:'peter@gmail.com'}
+}
+
+
+app.get('/user',(req,res)=>{
+    const username=req.query.user;
+
+    if(!username){
+        return res.status(400).json({message:"user parameter cannot be empty"})
+
+    }
+
+
+    const userdata=users[username];
+
+    if(userdata){
+        res.json({message:"user found",userDetails:userdata})
+    } else{
+        res.status(404).json({message:"user not found"})
+    }
+})
